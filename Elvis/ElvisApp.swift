823c9739c2a9d928039f2ac601221f274822305b
nbmcguire <57973784+nbmcguire@users.noreply.github.com>
2025-01-28 16:48:17 -0500

    Initial Commit

---
+++ Elvis/ElvisApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElvisApp.swift
+//  Elvis
+//
+//  Created by Noah McGuire on 1/28/25.
+//
+
+import SwiftUI
+
+@main
+struct ElvisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
